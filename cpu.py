cab0c160a6b91ee2874ac0293013ccfe98bd0f04
ashrdev <ashrdev@gmail.com>
2025-01-08 03:47:44 -0500

    initial cpu skeleton and vars created

--- cpu.py
+++ cpu.py
@@ -1,4 +1,44 @@
 import numpy as np
 class CPU:
-    def __init__():
+    def __init__(self):
+        self.memory = np.array(4096, dtype=np.uint8) # Memory is 4kb
+        self.display = np.array((32, 64), dtype=np.uint8)
+        self.pc = 0x050 # ranges from 0x0000 to 0x1000
+        self.I = 0x0000 #16 bit index register
+        self.delayTimer = 0x00 # 8 bit delay timer
+        self.soundTimer = 0x00 # 8 bit osund timer
+        self.registers = np.array(16, dtype=np.uint8) #Variable registers
+        self.load_font() #Load font into memory
+        self.stack = []
+    
+    def load_font(self):
+        CHIP8_FONTSET = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0,  # 0
+            0x20, 0x60, 0x20, 0x20, 0x70,  # 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,  # 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,  # 3
+            0x90, 0x90, 0xF0, 0x10, 0x10,  # 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,  # 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,  # 6
+            0xF0, 0x10, 0x20, 0x40, 0x40,  # 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,  # 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,  # 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90,  # A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,  # B
+            0xF0, 0x80, 0x80, 0x80, 0xF0,  # C
+            0xE0, 0x90, 0x90, 0x90, 0xE0,  # D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,  # E
+            0xF0, 0x80, 0xF0, 0x80, 0x80   # F
+        ]
+
+        for i, font in enumerate(CHIP8_FONTSET):
+            self.memory[self.pc + i] = font
+
+    def fetch(self):
+        opcode = (self.memory[self.pc] << 8) | self.memory[self.pc + 1]
+        self.pc += 2
+    
+    def decode(self, opcode):
         pass
+                
+                
